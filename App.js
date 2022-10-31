de8ec7b0fdf770126bbbf6816baafa169334c5fa
usmanhaider.vingo3tier <usmanhaider.vingo3tier@nxvt.com>
2022-10-31 18:05:25 +0500

    - git init

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import Tabbar from './components/Tabbar';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.backgroundStyle}>
+      <View style={styles.container}>
+        <Tabbar />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#ea3345',
+    justifyContent: 'flex-end',
+  },
+});
+
+export default App;
